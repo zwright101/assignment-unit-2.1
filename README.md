29777d06486cad1b7e903b0c4c9cefbd50cf02f1
mbmosman <mbMosman@users.noreply.github.com>
2021-03-01 11:43:19 -0600

    initial add

---
+++ README.md
@@ -0,0 +1,51 @@
+# Prime Tier 1 Week 2 - Part 1
+
+This week we introduced the programming language of the Internet, JavaScript. With JavaScript we can make our websites smart by adding interactivity and logic.
+
+## Topics Covered
+* Variables and Constants
+* Conditional statements
+
+
+## Assignment
+
+As always, start off by forking and cloning this repository from GitHub. Open the code up in Atom to get started.
+
+Remember `console.log()` is your friend and you can use it in conjunction with the browser console to see the output of your code as you go (just like you did to see your 'Hello World' last week!).
+
+### Files Provided
+
+This week you'll be completing the scripts in the following files. But note, that we have different levels of difficulty.
+
+- [ ] *index.html* (You can put any questions you have in this file)
+- [ ] *aboutMe.js* (Required Features, Stretch Goals)
+
+The aboutMe.js file contains comments with prompts on what code to write. Do not erase the comments, just write the code below each commented line.
+
+**NOTE:** The *aboutMe.js* file is already sourced into *index.html*. *describe.js* is for practicing explaining code -- no need to source into the html file.
+
+#### Levels of Difficulty
+
+Below is a brief explanation of:
+
+* what to expect when attempting each level
+* if they are required or not
+
+Level | Description
+--- | ---
+Required Features | required
+Stretch Goals | optional, stretches your understanding and may require additional research
+
+### Instructions
+Complete the `aboutMe.js` file.
+
+There are additional step by step instructions in the file -- please use `console.log` after every step to check your work! 
+Since some steps later in the file use results from earlier steps, you'll want to make sure things are right!
+
+As you get to conditionals, sometimes there are multiple possibilities for how our code could execute. 
+Try changing variables a bit to see if the code still works!
+
+### Assignment Submission
+Check in your repo, then turn in your work via the <a target="_blank" href="https://portal.primeacademy.io/#/student/assignments">Prime Academy Assignment Portal</a>, as usual and don't hesitate to hit up the Slack channel as needed!
+
+**REMINDER**: Make sure to answer the Slack discussion question!
---
+++ assignment/scripts/aboutMe.js
@@ -0,0 +1,54 @@
+// REQUIRED FEATURES:
+// 1 - Create a variable called `firstName` and assign it the value of your first name
+
+// 2 - Create a second variable called `lastName` and assign it the value of your last name
+
+// 3 - Create a third variable called `fullName`, assign it the value of your first and last name
+// (remember, you already have variables for this, can you use those?)
+
+// 4 - Console log the value of `fullName`
+
+// 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
+
+// 6 - Console log this sentence, adding in the variables you created above: 
+// 'My name is (full name), and I think (lucky number) is a winner!'.
+// Refer back to the videos if you need help with this one.
+
+// 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
+
+// 8 - Create a variable named `food`, and set its value to a string of your favorite food
+
+// 9 - Create a variable called `pets` and set it to the value of the number of pets you have
+
+// 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
+
+// 11 - Add two pets to your `pets` variable
+
+// 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
+
+// 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
+// if it's not true, console log "How about we stay home?"
+
+
+// 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
+// console log "Roll the dice!"
+
+// 15 - Write a conditional that console logs "I can have more pets!" 
+//if the value of `pets` is less than the value of `allowedPets`,
+// console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
+// and console logs "Oh no, I have too many pets!"
+// if the value of `pets` is greater than the value of `allowedPets`.
+
+// STRETCH GOALS:
+
+// 16 - Make a variable called `mostPets` and a conditional that
+// correctly checks the `pets` and `friendsPets` variables, and
+// assigns the highest value to `mostPets`. There's several possibilities --
+// be sure to think through all the scenarios. 
+// console.log `mostPets` after the conditional has run.
+
+// 17 - Rewrite question 16 with a `switch` statement. You'll need to do research!
+
+// 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
+
+
